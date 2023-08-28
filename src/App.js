d628ac4d756f9be7eaca3b3ca8c67a302321dff8
WebsiteAuthor <126562520+edncodeismad@users.noreply.github.com>
2023-08-28 19:33:34 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import img1 from './immagini/img1.png';
+import img2 from './immagini/img2.png';
+import img3 from './immagini/img3.png';
+import img4 from './immagini/img4.png';
+import img5 from './immagini/img5.png';
+import img6 from './immagini/img6.png';
+import img7 from './immagini/img7.png';
+import img8 from './immagini/img8.png';
+import img9 from './immagini/img9.png';
+import img10 from './immagini/img10.png';
+import img11 from './immagini/img11.png';
+import img12 from './immagini/img12.png';
+import img13 from './immagini/img13.png';
+import img14 from './immagini/img14.png';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Documenti</p>
+      <img src={img1}/>
+      <img src={img2}/>
+      <img src={img3}/>
+      <img src={img4}/>
+      <img src={img5}/>
+      <img src={img6}/>
+      <img src={img7}/>
+      <img src={img8}/>
+      <img src={img9}/>
+      <img src={img10}/>
+      <img src={img11}/>
+      <img src={img12}/>
+      <img src={img13}/>
+      <img src={img14}/>
+    </div>
+  );
+}
+
+export default App;
